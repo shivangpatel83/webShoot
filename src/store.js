4908475f44dd0634820c083c7c20ceb5f570fb71
shivang patel <shivang@nauggets.io>
2023-08-22 12:11:39 +0530

    add message

--- src/store.js
+++ src/store.js
@@ -4,4 +4,5 @@ import { rootReducer } from "./components/rootReducers";
 
 export const Store = createStore(rootReducer)
 
-//for api call i have implemented the sagas in this project
+//for api call i have implemented the sagas in this project but did not use just because i have to a single api call
+//for api calls we use sagas
